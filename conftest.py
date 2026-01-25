3c1041f25f1aa5c922bc44c25d52f2071c6ddfab
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-25 23:37:33 +0200

    update conftest

--- conftest.py
+++ conftest.py
@@ -1,34 +1,31 @@
 import pytest
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--headless", 
-        action="store_true", 
-        default=False, 
-        help="Запустить браузер в скрытом режиме (без GUI)"
-    )
-
 @pytest.fixture()
-def driver(request):
-    service_obj = Service(ChromeDriverManager().install())
-    
-    # Создаем объект опций
+def driver():
+    # Создаем объект настроек
     options = Options()
 
-    # Проверяем: если в терминале написали --headless
-    headless_mode = request.config.getoption("--headless")
-
-    if headless_mode:
+    # САМАЯ ВАЖНАЯ ЧАСТЬ
+    # Проверяем: "Я сейчас на Линуксе?" (GitHub Actions всегда Linux)
+    if sys.platform.startswith("linux"):
+        print("\nЗапуск на сервере (Linux) -> Включаем Headless!")
         options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox") # Обязательно для GitHub!
-        options.add_argument("--disable-dev-shm-usage") # Обязательно для GitHub!
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
     else:
+        # Иначе (Windows/Mac) -> Запускаем с окном
+        print("\nЗапуск локально -> Включаем GUI")
         options.add_argument("--start-maximized")
 
-    # ВАЖНО: передаем options в драйвер!
-    driver_obj = webdriver.Chrome(service=service_obj, options=options)
+    service = Service(ChromeDriverManager().install())
+    # Передаем options обязательно!
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    yield driver
+    driver.quit()
