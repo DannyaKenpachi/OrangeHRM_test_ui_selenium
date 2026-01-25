d25d70ff03388d0f5ab88e58e39b302c65ce7325
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-25 23:18:12 +0200

    update conftest

--- conftest.py
+++ conftest.py
@@ -3,6 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", 
+        action="store_true", 
+        default=False, 
+        help="Запустить браузер в скрытом режиме (без GUI)"
+    )
+    
 @pytest.fixture()
 def driver():
     service_obj = Service(ChromeDriverManager().install())
